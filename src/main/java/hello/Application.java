2d2af8f901aa8a2c08b755f5b1c2cf80759ffeae
debmitra <debmalya.mitra@outlook.com>
2017-10-04 18:50:09 +0000

    added configurations for mongodb

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class Application {
